98c76e399e18cd31bcb45d97a95a11d9a660cadb
Nistelq8 <aziz.almarzouq@gmail.com>
2022-07-18 19:05:15 +0300

    solved task

--- django_task/Restaurants/models.py
+++ django_task/Restaurants/models.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
